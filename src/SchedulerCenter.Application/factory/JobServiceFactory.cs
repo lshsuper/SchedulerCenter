49d3ac467a32a1fda001f9b7d7a87b5459551c52
liushuai <shuai.liu@xiaoyangedu.com>
2022-09-14 22:02:45 +0800

    feat hob工厂 Signed-off-by: liushuai <shuai.liu@xiaoyangedu.com>

---
+++ src/SchedulerCenter.Application/factory/JobServiceFactory.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.Configuration;
+using SchedulerCenter.Application.Services;
+using SchedulerCenter.Core.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchedulerCenter.Application.factory
+{
+  public  class JobServiceFactory
+    {
+
+        private IConfiguration _configuration { get; set; }
+     
+        private readonly IJobService _jobService;
+        private readonly IJobService _jobRemoteService;
+        public JobServiceFactory(IEnumerable<IJobService>jobServiceArr,IConfiguration configuration) {
+
+
+            _configuration = configuration;
+          
+            _jobService = jobServiceArr.FirstOrDefault(f=>f.GetType()==typeof(JobService));
+            _jobRemoteService = jobServiceArr.FirstOrDefault(f => f.GetType() == typeof(JobRemoteService));
+        }
+
+        /// <summary>
+        /// GetService 获取Service
+        /// </summary>
+        /// <param name="schedName"></param>
+        /// <returns></returns>
+        public async Task<IJobService> GetService(string schedName) {
+
+            //判断是远程还是本地
+
+            if (_configuration["SchedulerName"] == schedName) {
+                //本地调度
+                return  await _jobService.Init(schedName);
+               
+            }
+
+           return await _jobRemoteService.Init(schedName);
+
+        }
+
+        public async Task<IJobService> GetLocatService(string schedName)
+        {
+            //本地调度
+            return await _jobService.Init(schedName);           
+
+        }
+
+    }
+}
